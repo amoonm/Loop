e8b5aa69cc23d1700c5f89e9e4c32e7306c2977b
Amoon Mainali <amoonmainali8@gmail.com>
2021-09-13 16:49:44 -0400

    Created project for even numbers using loop

---
+++ src/main/java/com/mycompany/loopslab/Main.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.loopslab;
+
+/**
+ *
+ * @author amoonmainali
+ */
+public class Main {
+    public static void main(String[] args) {
+   
+        //for loop to print numbers from 0-100
+	for (int i = 0; i <= 100; i++) {
+	   //if statement to get even numbers between 0-100
+	   if (i % 2 == 0) {
+		System.out.println(i + " ");
+	   }
+	}
+    }
+}
